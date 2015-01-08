151e796dd48605e3fe390a0f19457abd79c1416a
mohit <mohit.11.arora@gmail.com>
2015-01-08 09:54:37 +0530

    changes

---
+++ environment-compliance.php
@@ -0,0 +1,88 @@
+<?php  include 'globalvar.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Thermal Power - Environment Clearances for Anuppur Phase 1</title>
+    
+	
+<?php  include 'meta.html';?>    
+</head><!--/head-->
+<body data-spy="scroll" data-target="#navbar" data-offset="0">
+<?php include 'header.html';?>
+    <section id ="recognition">
+		<div class="container">
+            <div class="box">
+                <div class="center gap">
+                    <h2>Environment Compliance</h2>
+					<div class="lead">
+<table width="974" border="0" cellspacing="0" cellpadding="0">
+  
+  <tr>
+    <td align="left" class="h-bor"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="211" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td><img src="images/spacer.gif" width="211" height="1" /></td>
+          </tr>
+          <tr>
+            <td align="left"><!--#include file="inc-leftlinks-thermal.asp"--></td>
+          </tr>
+        </table></td>
+        <td width="100%" valign="top" style="padding-left:15px;"><table border="0" width="95%" cellspacing="0" cellpadding="0" id="text">
+          
+          <tr>
+            <td valign="top" align="left" width="33%">
+			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<!--<tr>
+				  <td width="33%" valign="top"><img src="images/th1b.gif" class="impadbor" /></td>
+				  <td valign="top" width="33%"><img src="images/th1.gif" class="impadbor" /></td>
+				  
+				  </tr>-->
+				<tr>
+				  <td valign="top"><b>Annex A-EC <br/>Compliance Dec 14</b></td>
+				  <td valign="top"><b>EC Compliance Sep 2014.pdf</b></td>
+					
+				</tr>
+				<tr>
+				  <td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%">
+				    <tr>
+				      <td width="30"><img src="images/pdf.gif" width="25" height="26" /></td>
+				      <td><a target="_blank" href="pdfs/Annex-A-EC-Compliance-Dec-14.pdf"> Download in PDF format</a></td>
+				      </tr>
+				    </table></td>
+				  <td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="100%">
+				    <tr>
+				      <td width="30"><img src="images/pdf.gif" width="25" height="26" /></td>
+				      <td><a target="_blank" href="pdfs/EC-compliance-sep-2014.pdf"> Download in PDF format</a></td>
+				      </tr>
+				    </table></td>
+					
+				</tr>
+				<tr>
+				  <td valign="top">&nbsp;</td>
+				  <td valign="top">&nbsp;</td>
+				  <td valign="top">&nbsp;</td>
+				  </tr>
+			</table>
+<p>&nbsp;</p>
+                      <p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td align="left"><!--#include file="inc-footer.asp"--></td>
+  </tr>
+</table>
+					</div>
+					</div></div></div>
+	</section>
+    
+	
+<?php include 'footer.html';?>
+
+    
+
